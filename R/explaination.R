e10e8b95cb447661d55df69c745f130af91301fe
“andytai7” <andy.tai@stat.ubc.ca>
2024-10-03 11:38:33 -0700

    package development

---
+++ R/explaination.R
@@ -0,0 +1,21 @@
+#' Beta Data
+#'
+#' A dataset containing beta regression toy data.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{var1}{description of var1}
+#'   \item{var2}{description of var2}
+#' }
+"Beta"
+
+#' OLS Data
+#'
+#' A dataset containing ordinary least squares regression toy data.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{var1}{description of var1}
+#'   \item{var2}{description of var2}
+#' }
+"OLS"
