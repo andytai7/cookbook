cab0926b6c0906bbdfea6e9acb91d48809bf83e7
Andy Tai <andytai@CPSC-M-21944.lan>
2025-02-19 14:20:58 -0800

    changes to documentation

--- R/explaination.R
+++ R/explaination.R
@@ -2,10 +2,19 @@
 #'
 #' A dataset containing beta regression toy data.
 #'
-#' @format A data frame with X rows and Y variables:
+#' @format A data frame with 1000 rows and 11 variables:
 #' \describe{
-#'   \item{var1}{description of var1}
-#'   \item{var2}{description of var2}
+#'   \item{Final Grade (%)}{Description of Final Grade (%)}
+#'   \item{Café Location}{Description of Café Location}
+#'   \item{Study Time}{Description of Study Time}
+#'   \item{Year in School}{Description of Year in School}
+#'   \item{Hobbies}{Description of Hobbies}
+#'   \item{Primary Sport}{Description of Primary Sport}
+#'   \item{Living Arrangement}{Description of Living Arrangement}
+#'   \item{Part-time Work Hours}{Description of Part-time Work Hours}
+#'   \item{Study Group Participation}{Description of Study Group Participation}
+#'   \item{Hours of Sleep per Night}{Description of Hours of Sleep per Night}
+#'   \item{Has a Tutor}{Description of Has a Tutor}
 #' }
 "Beta"
 
@@ -13,9 +22,19 @@
 #'
 #' A dataset containing ordinary least squares regression toy data.
 #'
-#' @format A data frame with X rows and Y variables:
+#' @format A data frame with 1000 rows and 11 variables:
 #' \describe{
-#'   \item{var1}{description of var1}
-#'   \item{var2}{description of var2}
+#'   \item{Has_Job}{Description of Has_Job}
+#'   \item{Year_of_Study}{Description of Year_of_Study}
+#'   \item{Financially_Dependent}{Description of Financially_Dependent}
+#'   \item{Monthly_Allowance}{Description of Monthly_Allowance}
+#'   \item{Cooks_at_Home}{Description of Cooks_at_Home}
+#'   \item{Living_Situation}{Description of Living_Situation}
+#'   \item{Housing_Type}{Description of Housing_Type}
+#'   \item{Goes_Out_Spends_Money}{Description of Goes_Out_Spends_Money}
+#'   \item{Drinks_Alcohol}{Description of Drinks_Alcohol}
+#'   \item{Net_Money}{Description of Net_Money}
+#'   \item{Monthly_Earnings}{Description of Monthly_Earnings}
 #' }
 "OLS"
+
